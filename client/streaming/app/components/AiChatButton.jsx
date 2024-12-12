e54ab799bae3eadc5b7a2db3a2e0c324e45bf14b
Clay0010 <ahmedayad328@gmail.com>
2024-12-12 14:53:03 +0300

    modify chat style

--- client/streaming/app/components/AiChatButton.jsx
+++ client/streaming/app/components/AiChatButton.jsx
@@ -6,9 +6,10 @@ const AiChatButton = () => {
   return (
     <Link href="/ai-chat">
       <span
-        className="fixed bottom-6 right-6 z-50 bg-gray-800 text-white p-4 rounded-full shadow-lg hover:bg-gray-600 transition-transform transform  flex items-center justify-center"
+        className="fixed bottom-6 right-6 z-50 bg-gray-800 text-white text-sm p-2 py-3 rounded-full shadow-lg hover:bg-gray-600 transition-transform transform  flex items-center justify-center"
         aria-label="Chat with AI"
       >
+        <span className="mr-2">Chat With AI</span>
         <i className="fas fa-comment-dots text-lg"></i>
       </span>
     </Link>
